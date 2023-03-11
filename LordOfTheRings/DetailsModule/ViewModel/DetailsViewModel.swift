1d0d61856ce709751f743fc3d3a3c2bc79500837
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-03-11 15:34:08 +0300

    Network layer fix, UI Fix

--- LordOfTheRings/DetailsModule/ViewModel/DetailsViewModel.swift
+++ LordOfTheRings/DetailsModule/ViewModel/DetailsViewModel.swift
@@ -10,26 +10,30 @@ import Foundation
 
 protocol DetailsViewModelProtocol: AnyObject {
     var characterData: Character? { get set }
+    var characterDetails: CharacterDetails? { get }
     var quote: Quote? { get }
     var movie: String { get }
     var reload: (() -> ())? { get set }
-    var networkService: NetworkServiceProtocol { get set }
+    var networkService: NetworkServiceProtocol { get }
+    var characterBirthDeath: String? { get set }
     func fetchData()
 }
 
 class DetailsViewModel: DetailsViewModelProtocol {
     
     var quote: Quote?
+    var characterBirthDeath: String?
     var characterData: Character?
-    private var quotes: [Quote] = []
+    var characterDetails: CharacterDetails?
     var reload: (() -> ())?
     var isLoading: Bool = false
     var networkService: NetworkServiceProtocol
+    let dispatchGroup = DispatchGroup()
     var movie: String {
         
         guard let quote = quote else { return "Sorry, no data"}
         if quote.movie == MoviesEnum.bookOne.rawValue {
-            return "The Fellowship of The Ring" 
+            return "The Fellowship of The Ring"
         }
         if quote.movie == MoviesEnum.bookTwo.rawValue {
             return "The Two Towers"
@@ -46,22 +50,36 @@ class DetailsViewModel: DetailsViewModelProtocol {
     }
     
     func fetchData() {
-        guard !isLoading else { return}
+        guard !isLoading else { return }
         isLoading = true
-        networkService.fetchCharacterQuotes(characterId: characterData!.id , completion: { [self] result in
-            switch result {
-            case .success(let quotes):
-                guard let quotes = quotes else { return }
-                if quotes.isEmpty {
-                    return
+        dispatchGroup.enter()
+            networkService.fetchCharacterQuotes(characterId: characterData!.id , completion: { [self] result in
+                dispatchGroup.leave()
+                switch result {
+                case .success(let quotes):
+                    self.quote = quotes?.randomElement()
+                case .failure(let error):
+                    print (error)
                 }
-                self.quotes = quotes
-                self.quote = self.quotes.randomElement()!
-                self.reload?()
-            case .failure(let error):
-                print (error)
+            })
+        dispatchGroup.enter()
+            networkService.fetchCharacterDetails(characterId: characterData!.id) { [self] result in
+                dispatchGroup.leave()
+                switch result {
+                case .success(let character):
+                    guard let character = character else { return }
+                    if character.isEmpty { return }
+                    self.characterDetails = character.first
+                    self.characterBirthDeath = "\(characterDetails!.birth) - \(characterDetails!.death)"
+                case .failure(let error):
+                    print (error)
+                }
             }
-        })
+        
+        dispatchGroup.notify(queue: .main) {
+            self.reload?()
+            
+        }
         
     }
 }
--- LordOfTheRings/Helpers/Enums.swift
+++ LordOfTheRings/Helpers/Enums.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+    case emptyResponse
+}
+
 enum MoviesEnum: String {
     case bookOne = "5cd95395de30eff6ebccde5c"
     case bookTwo = "5cd95395de30eff6ebccde5b"
